d17d2ace8492d5e6aa9df34233340c0a537ed293
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-07 15:31:36 +0200

    Update projects title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,7 @@ export const Projects = () => {
   return (
     <>
       <div className="min-container">
-        <h2 className="min-title">Projects</h2>
-        <h3 className="min-subtitle">Discover my most recent projects</h3>
+        <h2 className="min-title">Featured Projects</h2>
       </div>
     </>
   );
