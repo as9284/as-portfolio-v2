ca60b3bcb535942cabb113943812e884a7a68bf2
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-03-26 17:48:43 +0200

    Further improvements to mobile responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const techStack = [
 
 export const Home = () => {
   return (
-    <div className="relative w-full min-h-dvh bg-black text-white flex flex-col justify-center items-center p-4 text-center select-none">
+    <div className="relative w-full min-h-dvh bg-black text-white flex flex-col justify-between p-4 text-center select-none overflow-auto">
       <div
         style={{
           backgroundColor: "#000000",
@@ -31,31 +31,31 @@ export const Home = () => {
         }}
         className="absolute inset-0 w-full h-full z-0"
       />
-      <div className="absolute w-full top-0 right-0 flex justify-between items-center p-4">
-        <h3 className="text-2xl md:text-3xl font-semibold">Anthony Saliba</h3>
-        <div className="flex gap-4 md:gap-8 text-3xl">
+
+      {/* Header */}
+      <div className="w-full flex justify-between items-center p-4 z-10">
+        <h3 className="text-xl md:text-3xl font-semibold">Anthony Saliba</h3>
+        <div className="flex gap-4 md:gap-8 text-2xl md:text-3xl">
           <FaLinkedin className="cursor-pointer" />
           <FaGithub className="cursor-pointer" />
           <MdEmail className="cursor-pointer" />
         </div>
       </div>
 
-      <div className="w-full flex flex-col justify-center items-center z-10">
+      {/* Main Content */}
+      <div className="flex flex-col justify-center items-center flex-grow z-10 p-4">
         <h1 className="text-3xl md:text-5xl font-bold">Frontend Developer</h1>
         <h5 className="text-base md:text-xl font-semibold uppercase">
           Portfolio
         </h5>
-      </div>
-
-      <hr className="w-60 md:w-96 border-1 my-4 z-10" />
-
-      <div className="w-full flex justify-center items-center">
+        <hr className="w-60 md:w-96 border-1 my-4" />
         <button className="btn-modern">Featured Projects</button>
       </div>
 
-      <div className="absolute bottom-0 w-full flex flex-col justify-center items-center">
+      {/* Tech Stack Section */}
+      <div className="w-full flex flex-col justify-center items-center z-10">
         <h3 className="text-lg md:text-2xl font-semibold">Tech I Use</h3>
-        <div className="w-full flex justify-center items-center gap-4 md:gap-8 text-4xl py-4">
+        <div className="flex flex-wrap justify-center items-center gap-4 md:gap-8 text-4xl py-4">
           {techStack.map((tech, index) => (
             <img
               key={index}
