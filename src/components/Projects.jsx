ca7782ebc2fb0fea8dad35ec505627fd288c969d
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-03-27 15:36:16 +0200

    Add loading between each modal opening

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import projectsData from "../data/projectsData.json";
+
+import { Square } from "ldrs/react";
+import "ldrs/react/Square.css";
+
 import wirestorm from "../assets/img/wirestorm.webp";
 import memoir from "../assets/img/memoir.webp";
 import cloud from "../assets/img/cloud.webp";
@@ -18,6 +22,19 @@ const images = {
 
 export const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    if (selectedProject) {
+      const timer = setTimeout(() => {
+        setShowContent(true);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    } else {
+      setShowContent(false);
+    }
+  }, [selectedProject]);
 
   return (
     <div className="w-full flex flex-wrap flex-col md:flex-row justify-center items-center gap-12 md:gap-28 z-10">
@@ -46,51 +63,66 @@ export const Projects = () => {
       {selectedProject && (
         <div className="fixed inset-0 flex items-center justify-center bg-black/40 backdrop-blur-md z-50 p-2">
           <div className="relative w-full md:w-3/4 lg:w-1/2 h-[80%] md:h-[90%] bg-black border-2 border-white text-white flex flex-col justify-between items-center">
-            {/* Project Image */}
-            <img
-              src={images[selectedProject.src.split(".")[0]]}
-              alt={selectedProject.title}
-              className="w-full object-contain mb-4"
-            />
+            {!showContent ? (
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <Square
+                  size="50"
+                  stroke="4"
+                  strokeLength="0.25"
+                  bgOpacity="0.3"
+                  speed="1"
+                  color="white"
+                />
+              </div>
+            ) : (
+              <>
+                {/* Project Image */}
+                <img
+                  src={images[selectedProject.src.split(".")[0]]}
+                  alt={selectedProject.title}
+                  className="w-full object-contain mb-4"
+                />
 
-            {/* Project Details */}
-            <h2 className="text-2xl md:text-3xl font-bold">
-              {selectedProject.title}
-            </h2>
-            <p className="text-base md:text-xl pt-2 px-4">
-              {selectedProject.description}
-            </p>
+                {/* Project Details */}
+                <h2 className="text-2xl md:text-3xl font-bold">
+                  {selectedProject.title}
+                </h2>
+                <p className="text-base md:text-xl pt-2 px-4">
+                  {selectedProject.description}
+                </p>
 
-            {/* Buttons */}
-            <div className="w-full flex flex-col md:flex-row justify-center items-center gap-4 my-6">
-              {selectedProject.title === "Wirestorm Networks" ? (
-                <>
-                  <button
-                    className="btn-push"
-                    onClick={() => setSelectedProject(null)}
-                  >
-                    Close
-                  </button>
-                </>
-              ) : (
-                <>
-                  <button
-                    className="btn-push"
-                    onClick={() =>
-                      window.open(selectedProject.github, "_blank")
-                    }
-                  >
-                    Github
-                  </button>
-                  <button
-                    className="btn-push"
-                    onClick={() => setSelectedProject(null)}
-                  >
-                    Close
-                  </button>
-                </>
-              )}
-            </div>
+                {/* Buttons */}
+                <div className="w-full flex flex-col md:flex-row justify-center items-center gap-4 my-6">
+                  {selectedProject.title === "Wirestorm Networks" ? (
+                    <>
+                      <button
+                        className="btn-push"
+                        onClick={() => setSelectedProject(null)}
+                      >
+                        Close
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        className="btn-push"
+                        onClick={() =>
+                          window.open(selectedProject.github, "_blank")
+                        }
+                      >
+                        Github
+                      </button>
+                      <button
+                        className="btn-push"
+                        onClick={() => setSelectedProject(null)}
+                      >
+                        Close
+                      </button>
+                    </>
+                  )}
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
