d21b73126521890bc30271ff1aaeff20973b43bc
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-03-27 11:14:26 +0200

    Fix sizing issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ export const Hero = () => {
     <>
       <div className="flex flex-col justify-center items-center flex-grow z-10 p-4">
         <div className="bg-black border-2 border-white p-8">
-          <h1 className="text-3xl md:text-5xl font-bold">Frontend Developer</h1>
-          <h5 className="text-base md:text-xl font-semibold uppercase">
+          <h1 className="text-2xl md:text-5xl font-bold">Frontend Developer</h1>
+          <h5 className="text-sm md:text-xl font-semibold uppercase">
             Portfolio
           </h5>
-          <hr className="w-60 md:w-96 border-1 my-4" />
+          <hr className="w-full border-1 my-4" />
           <button className="btn-push">Featured Projects</button>
         </div>
       </div>
