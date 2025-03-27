602c28fa18248916d93ad80f54d949b60193122f
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-03-27 11:49:21 +0200

    Revert box design changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center flex-grow z-10 p-4">
-        <div className="bg-black border-2 border-white p-8 box">
+        <div className="bg-black border-2 border-white p-8">
           <h1 className="text-2xl md:text-5xl font-bold">Frontend Developer</h1>
           <h5 className="text-sm md:text-xl font-semibold uppercase">
             Portfolio
