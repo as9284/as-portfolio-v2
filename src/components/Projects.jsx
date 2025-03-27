b23c0b85837f203845d98a54c69d565a389bd476
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-03-27 12:43:33 +0200

    Add the projects data file and make project previewing work

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,24 @@
 import React from "react";
-
-const projects = [
-  { name: "Wirestorm Networks" },
-  { name: "Memoir" },
-  { name: "Sky Cloud" },
-  { name: "Holo Movies" },
-  { name: "Space Rise" },
-  { name: "Clean Link" },
-];
+import projectsData from "../data/projectsData.json";
 
 export const Projects = () => {
   return (
-    <>
-      <div className="w-full flex flex-wrap flex-col md:flex-row justify-center items-center gap-12 md:gap-28 z-10">
-        {projects.map((project, index) => (
-          <div
-            key={index}
-            className="w-72 h-60 bg-black border-2 border-white flex flex-col justify-center items-center gap-4"
+    <div className="w-full flex flex-wrap flex-col md:flex-row justify-center items-center gap-12 md:gap-28 z-10">
+      {projectsData.map((project) => (
+        <div
+          key={project.id}
+          className="w-72 h-60 bg-black border-2 border-white flex flex-col justify-center items-center gap-4"
+        >
+          <h4 className="text-xl font-bold">{project.title}</h4>
+          <button className="btn-push">Project Details</button>
+          <button
+            className="btn-push"
+            onClick={() => window.open(project.preview, "_blank")}
           >
-            <h4 className="text-xl font-bold">{project.name}</h4>
-            <button className="btn-push">Project Details</button>
-            <button className="btn-push">Project Preview</button>
-          </div>
-        ))}
-      </div>
-    </>
+            Project Preview
+          </button>
+        </div>
+      ))}
+    </div>
   );
 };
