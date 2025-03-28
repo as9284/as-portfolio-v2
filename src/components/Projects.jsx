b3cba9fd546c1266caf4857bc893b2af6a7e4c60
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-03-28 14:21:48 +0200

    Update project positioning to use grid instead of flex

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ export const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
 
   return (
-    <div className="w-full flex flex-wrap flex-col md:flex-row justify-center items-center gap-12 md:gap-28 z-10">
+    <div className="w-full place-items-center justify-items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 z-10">
       {projectsData.map((project) => (
         <div
           key={project.id}
