dd9dbaf7a423a4e3d338781e0be7fc1028c2aaff
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-04-04 14:38:09 +0300

    Add hover tooltips on icons

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,17 +3,46 @@ import React from "react";
 const images = import.meta.glob("../assets/svg/*", { eager: true });
 
 const techStack = [
-  { src: images["../assets/svg/html5.svg"].default, alt: "HTML5 logo" },
-  { src: images["../assets/svg/css3.svg"].default, alt: "CSS3 logo" },
-  { src: images["../assets/svg/js.svg"].default, alt: "JavaScript logo" },
-  { src: images["../assets/svg/node.svg"].default, alt: "Node.js logo" },
-  { src: images["../assets/svg/nextjs.svg"].default, alt: "Next.js logo" },
-  { src: images["../assets/svg/react.svg"].default, alt: "React.js logo" },
+  {
+    src: images["../assets/svg/html5.svg"].default,
+    alt: "HTML5 logo",
+    name: "HTML5",
+  },
+  {
+    src: images["../assets/svg/css3.svg"].default,
+    alt: "CSS3 logo",
+    name: "CSS3",
+  },
+  {
+    src: images["../assets/svg/js.svg"].default,
+    alt: "JavaScript logo",
+    name: "JavaScript",
+  },
+  {
+    src: images["../assets/svg/node.svg"].default,
+    alt: "Node.js logo",
+    name: "Node.js",
+  },
+  {
+    src: images["../assets/svg/nextjs.svg"].default,
+    alt: "Next.js logo",
+    name: "Next.js",
+  },
+  {
+    src: images["../assets/svg/react.svg"].default,
+    alt: "React.js logo",
+    name: "React.js",
+  },
   {
     src: images["../assets/svg/tailwind.svg"].default,
     alt: "Tailwind CSS logo",
+    name: "Tailwind CSS",
   },
-  { src: images["../assets/svg/figma.svg"].default, alt: "Figma logo" },
+  {
+    src: images["../assets/svg/figma.svg"].default,
+    alt: "Figma logo",
+    name: "Figma",
+  },
 ];
 
 export const Tech = () => {
@@ -23,14 +52,18 @@ export const Tech = () => {
         <h3 className="text-base md:text-xl font-semibold">Tech I Use</h3>
         <div className="flex flex-wrap justify-center items-center gap-4 md:gap-10 text-4xl py-4">
           {techStack.map((tech, index) => (
-            <img
-              key={index}
-              src={tech.src}
-              alt={tech.alt}
-              className={`w-6 md:w-10 ${
-                tech.alt === "Figma logo" ? "h-6 md:h-10" : ""
-              }`}
-            />
+            <div key={index} className="relative group">
+              <img
+                src={tech.src}
+                alt={tech.alt}
+                className={`w-6 md:w-10 ${
+                  tech.alt === "Figma logo" ? "h-6 md:h-10" : ""
+                }`}
+              />
+              <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 opacity-0 group-hover:opacity-100 transition-opacity bg-black border-2 border-white text-white text-sm py-1 px-2 text-nowrap">
+                {tech.name}
+              </div>
+            </div>
           ))}
         </div>
       </div>
